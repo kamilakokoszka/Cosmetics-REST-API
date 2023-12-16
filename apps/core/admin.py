668248ede3a7ff9b2895711604112b29e1bf5933
kamila2508 <kamila3602@gmail.com>
2023-12-16 16:14:35 +0100

    feat: add customization for User model in admin site

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,46 @@
-from django.contrib import admin
+"""
+Django admin customization.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from ..core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'username']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+         ),
+        ('Important dates', {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'username',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+    search_fields = ('email',)
+
+
+admin.site.register(models.User, UserAdmin)
