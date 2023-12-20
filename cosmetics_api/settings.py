ca6324b9f1bdd6c8936d38f6ff89f96ed60ac20c
kamila2508 <kamila3602@gmail.com>
2023-12-20 12:05:29 +0100

    feat: add drf_spectacular to INSTALLED_APPS, configure DEFAULT_SCHEMA_CLASS

--- cosmetics_api/settings.py
+++ cosmetics_api/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.core'
+    'drf_spectacular',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
