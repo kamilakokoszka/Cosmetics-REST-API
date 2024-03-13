1eb2bc5511df85866f36967166f7273c5596134a
kamila2508 <kamila3602@gmail.com>
2024-03-13 20:38:06 +0100

    feat: add create user url

---
+++ apps/user/urls.py
@@ -0,0 +1,13 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from .views import CreateUserView
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create')
+]
