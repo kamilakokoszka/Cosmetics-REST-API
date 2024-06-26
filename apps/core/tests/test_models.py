1320081c0c13c0f04c94154b3726bd5f4dcf9f2a
kamila2508 <kamila3602@gmail.com>
2024-06-26 21:01:02 +0200

    test: add tests for models

--- apps/core/tests/test_models.py
+++ apps/core/tests/test_models.py
@@ -4,6 +4,13 @@ Tests for models.
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from ..models import (
+    Brand,
+    Group,
+    Category,
+    Store,
+    Product
+)
 
 
 def create_user(email='user1@example.com', password='Testpass123'):
@@ -40,3 +47,103 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_brand(self):
+        """Test creating a brand is successful."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'Testpass123',
+        )
+        brand = Brand.objects.create(
+            user=user,
+            name='Test brand',
+        )
+
+        self.assertEqual(str(brand), brand.name)
+
+    def test_create_group(self):
+        """Test creating a group is successful."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'Testpass123',
+        )
+        group = Group.objects.create(
+            user=user,
+            name='Test group',
+        )
+
+        self.assertEqual(str(group), group.name)
+
+    def test_create_category(self):
+        """Test creating a category is successful."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'Testpass123',
+        )
+        group = Group.objects.create(
+            user=user,
+            name='Test group',
+        )
+        category = Category.objects.create(
+            user=user,
+            name='Test category',
+            group=group
+        )
+
+        self.assertEqual(str(category), category.name)
+        self.assertEqual(category.group.name, group.name)
+
+    def test_create_store(self):
+        """Test creating a store is successful."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'Testpass123',
+        )
+        store = Store.objects.create(
+            user=user,
+            name='Test store'
+        )
+
+        self.assertEqual(str(store), store.name)
+
+    def test_create_product(self):
+        """Test creating a product is successful."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'Testpass123',
+        )
+        brand = Brand.objects.create(
+            user=user,
+            name='Test brand',
+        )
+        group = Group.objects.create(
+            user=user,
+            name='Test group',
+        )
+        category = Category.objects.create(
+            user=user,
+            name='Test category',
+            group=group
+        )
+        store = Store.objects.create(
+            user=user,
+            name='Test store'
+        )
+        product = Product.objects.create(
+            user=user,
+            name='Test product',
+            brand=brand,
+            group=group,
+            category=category,
+            price=15.00,
+            ingredients='Test, test, test',
+            capacity=50,
+            unit=2,
+            is_available=True,
+            is_favourite=False
+        )
+        product.stores.add(store)
+
+        self.assertEqual(str(product), product.name)
+        self.assertEqual(product.brand.name, brand.name)
+        self.assertEqual(product.category.group.name, group.name)
