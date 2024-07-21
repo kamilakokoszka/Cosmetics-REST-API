8d38cf56fc772a4f00e193429140e02b94a34890
kamila2508 <kamila3602@gmail.com>
2024-07-21 19:33:30 +0200

    feat: add token authentication

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,3 +1,18 @@
 """
 URL mappings for the user API.
 """
+from django.urls import include, path
+from rest_framework import routers
+from .views import (
+    BrandViewSet,
+)
+
+
+router = routers.DefaultRouter()
+router.register(r'brands', BrandViewSet, basename='brand')
+
+app_name = 'product'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,12 +1,19 @@
 """
 Views for the user API.
 """
-
+from drf_spectacular.utils import extend_schema, OpenApiResponse
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
-from apps.user.serializers import UserSerializer
+from apps.user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+    TokenCreateViewResponseSerializer
+)
 
 
 class UserRegisterView(CreateAPIView):
@@ -16,3 +23,29 @@ class UserRegisterView(CreateAPIView):
         permissions.AllowAny
     ]
     serializer_class = UserSerializer
+
+
+@extend_schema(
+    request=AuthTokenSerializer,
+    responses={
+        200: OpenApiResponse(
+            response=TokenCreateViewResponseSerializer
+        )
+    }
+)
+class TokenCreateView(ObtainAuthToken):
+    """Create a new auth token for user."""
+
+    def post(self, request, *args, **kwargs):
+        serializer = AuthTokenSerializer(data=request.data,
+                                         context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        if not user:
+            return Response({'error': 'Invalid credentials'}, status=400)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'username': user.username,
+            'email': user.email
+        })
