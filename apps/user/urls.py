c6494206506dc2f52a4e3fb2c48b9b13ed9f42e4
kamila2508 <kamila3602@gmail.com>
2024-07-21 19:38:20 +0200

    feat: add token url

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -3,11 +3,12 @@ URL mappings for the user API.
 """
 from django.urls import path
 
-from .views import UserRegisterView
+from .views import UserRegisterView, TokenCreateView
 
 
 app_name = 'user'
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='user-register')
+    path('register/', UserRegisterView.as_view(), name='user-register'),
+    path('token/', TokenCreateView.as_view(), name='token')
 ]
